187bdd7d57f3537743f3ee3f8f81ae13d47c8458
ander <ander.fernandezla@elorrieta-errekamari.com>
2019-04-03 21:13:01 +0200

    as

---
+++ src/vista/Ventana7.java
@@ -0,0 +1,48 @@
+package vista;
+
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/**
+ * 
+ * @author oier, ander, aintzane
+ *
+ */
+public class Ventana7 extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JLabel lblEskerrikAsko = new JLabel("Eskerrik asko erosteagatik ");
+	private JLabel lblZureTxartelaOrain = new JLabel("Zure txartela orain aterako da");
+	private JLabel icono = new JLabel();
+
+	public Ventana7() {
+		
+		
+		this.setBounds(275,100,700,600);
+		getContentPane().setLayout(null);
+		
+		//LABEL ESKERRIK ASKO
+		lblEskerrikAsko.setFont(new Font("Arial", Font.PLAIN, 46));
+		lblEskerrikAsko.setBounds(70, 95, 565, 78);
+		getContentPane().add(lblEskerrikAsko);
+		
+		//LABEL ZURE TXARTELA ORAIN
+		lblZureTxartelaOrain.setFont(new Font("Arial", Font.PLAIN, 41));
+		lblZureTxartelaOrain.setBounds(70, 198, 555, 45);
+		getContentPane().add(lblZureTxartelaOrain);
+		
+		
+		
+		icono.setBounds(194, 306, 307, 141);
+		getContentPane().add(icono);
+		icono.setIcon(new ImageIcon(Ventana7.class.getResource("/argazkiak/termibus.png")));
+		
+		
+		
+	}
+}
