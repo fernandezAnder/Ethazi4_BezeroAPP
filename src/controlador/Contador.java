bfb8472e1cb8a229e7184de3f8db70d38494074d
ander <ander.fernandezla@elorrieta-errekamari.com>
2019-04-03 21:12:46 +0200

    ordainketa pantaila

---
+++ src/controlador/Contador.java
@@ -0,0 +1,33 @@
+package controlador;
+
+import vista.Ventana7;
+
+/**
+ * 
+ * @author oier, ander, aintzane
+ *
+ */
+public class Contador extends Thread {
+
+	
+	@Override
+    public void run(){
+		Ventana7 ventana7= new Ventana7();
+		ventana7.setVisible(true);
+    	for (int i=0;i<=5;i++) {
+        	try {
+				Thread.sleep(1000);
+
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}	
+    	}
+    	ventana7.dispose();
+    	Metodoak.lehenengoLehioa();
+    }
+    	
+	
+}
+
+
+
