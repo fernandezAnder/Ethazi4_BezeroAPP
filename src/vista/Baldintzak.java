4c019d140b1a920a7fa35be5313bae0efa9a5e68
urtzi.etxebarria <urtzi.etxebarriaar@elorrieta-errekamari.com>
2019-05-06 12:03:57 +0200

    RHDFH

--- src/vista/Baldintzak.java
+++ src/vista/Baldintzak.java
@@ -13,6 +13,7 @@ import javax.swing.JRadioButton;
 import javax.swing.JLabel;
 import javax.swing.JCheckBox;
 import javax.swing.JTextArea;
+import javax.swing.border.MatteBorder;
 
 public class Baldintzak extends JFrame {
 
@@ -39,35 +40,35 @@ public class Baldintzak extends JFrame {
 	 */
 	public Baldintzak() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 500);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(135, 206, 235));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new MatteBorder(20, 10, 20, 10, (Color) new Color(70, 130, 180)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("ONARTU");
 		btnNewButton.setFont(new Font("Arial Narrow", Font.BOLD, 20));
-		btnNewButton.setBounds(314, 219, 110, 33);
+		btnNewButton.setBounds(426, 381, 110, 33);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("ATZERA");
 		btnNewButton_1.setFont(new Font("Arial Narrow", Font.BOLD, 20));
-		btnNewButton_1.setBounds(10, 220, 97, 31);
+		btnNewButton_1.setBounds(51, 382, 97, 31);
 		contentPane.add(btnNewButton_1);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Baldintzak onartzen ditut");
 		chckbxNewCheckBox.setFont(new Font("Arial Narrow", Font.BOLD, 12));
-		chckbxNewCheckBox.setBounds(138, 219, 141, 30);
+		chckbxNewCheckBox.setBounds(218, 384, 141, 30);
 		contentPane.add(chckbxNewCheckBox);
 		
 		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 51, 414, 157);
+		textArea.setBounds(50, 78, 486, 278);
 		contentPane.add(textArea);
 		
 		JLabel lblNewLabel = new JLabel("BALDINTZAK ETA KONDIZIOAK");
 		lblNewLabel.setFont(new Font("Arial Narrow", Font.BOLD, 20));
-		lblNewLabel.setBounds(90, 21, 261, 14);
+		lblNewLabel.setBounds(160, 36, 261, 24);
 		contentPane.add(lblNewLabel);
 	}
 }
