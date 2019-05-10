0567594d26238bcb104e491f4e0e4c593e7a5aad
urtzi.etxebarria <urtzi.etxebarriaar@elorrieta-errekamari.com>
2019-05-10 09:14:22 +0200

    DFGDF

--- src/vista/Amaiera.java
+++ src/vista/Amaiera.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Amaiera extends JFrame {
 
@@ -34,12 +36,27 @@ public class Amaiera extends JFrame {
 	 */
 	public Amaiera() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 500);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(135, 206, 250));
 		contentPane.setBorder(new MatteBorder(20, 10, 20, 10, (Color) new Color(70, 130, 180)));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ESKERRIK ASKO");
+		lblNewLabel.setFont(new Font("Arial Narrow", Font.BOLD, 40));
+		lblNewLabel.setBounds(139, 80, 320, 79);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("BIDAI ON");
+		lblNewLabel_1.setFont(new Font("Arial Narrow", Font.BOLD, 40));
+		lblNewLabel_1.setBounds(204, 158, 159, 56);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("ERABILTZEAGATIK");
+		lblNewLabel_2.setFont(new Font("Arial Narrow", Font.BOLD, 40));
+		lblNewLabel_2.setBounds(127, 219, 342, 79);
+		contentPane.add(lblNewLabel_2);
 	}
 
 }
--- src/vista/OngiEtorri.java
+++ src/vista/OngiEtorri.java
@@ -8,6 +8,9 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.border.MatteBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class OngiEtorri extends JFrame {
 
@@ -34,12 +37,22 @@ public class OngiEtorri extends JFrame {
 	 */
 	public OngiEtorri() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 500);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(135, 206, 250));
 		contentPane.setBorder(new MatteBorder(20, 10, 20, 10, (Color) new Color(70, 130, 180)));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblOngiEtorri = new JLabel("ONGI ETORRI");
+		lblOngiEtorri.setFont(new Font("Arial Narrow", Font.BOLD, 70));
+		lblOngiEtorri.setBounds(82, 49, 397, 141);
+		contentPane.add(lblOngiEtorri);
+		
+		JButton btnNewButton = new JButton("BIDAI ON");
+		btnNewButton.setBackground(new Color(70, 130, 180));
+		btnNewButton.setFont(new Font("Arial Narrow", Font.BOLD, 60));
+		btnNewButton.setBounds(93, 246, 368, 150);
+		contentPane.add(btnNewButton);
 	}
-
 }
